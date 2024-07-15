b146d8339cefc255add5a1c0fcbc458eb85a8fe1
Ben Gramling <bgramling18@gmail.com>
2024-07-14 19:45:34 -0500

    first commit

---
+++ src/main/java/com/arbriver/arbdelta/ArbdeltaApplication.java
@@ -0,0 +1,13 @@
+package com.arbriver.arbdelta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArbdeltaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArbdeltaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arbriver/arbdelta/ArbdeltaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arbriver.arbdelta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArbdeltaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
